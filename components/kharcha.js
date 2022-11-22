4414b0a14eeec02d6a892c110ac12b3f3307f56d
Nikunj Mavani <nikung.bvminfotech@gmail.com>
2022-11-23 00:00:07 +0530

    master

---
+++ components/kharcha.js
@@ -0,0 +1,96 @@
+import Link from 'next/link'
+import React from 'react'
+import Add_Master_Style from '../styles/add_master.module.css'
+
+function Kharcha(props) {
+    return (
+        <section>
+            {
+                props.all_list ?
+                    <div style={ { width: "auto", margin: "auto" } } className='card'>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Kharcha type</th>
+                                    <th>Group name</th>
+                                    <th style={ { textAlign: "center" } } colSpan="2">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    props.party_list.map((ele, index) => {
+                                        return (
+                                            <tr key={ index }>
+                                                <td>{ ele.id }</td>
+                                                <td>{ ele.kharcha_name }</td>
+                                                <td>{ ele.kharcha_group }</td>
+                                                <td style={ { padding: "0" } }>
+                                                    <button onClick={ () => props.delete_data(ele.id) } className='warning'>Delete</button>
+                                                </td>
+                                                <td style={ { padding: "10px" } }>
+                                                    <button onClick={ () => props.edit_data(ele.id) }>Edit</button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        <button
+                            onClick={ props.close_list }
+                            style={ {
+                                margin: "auto",
+                                display: "flex",
+                                alignItems: "center"
+                            } } className='secondary'>Close</button>
+                    </div>
+                    :
+                    <div className={ `card ${Add_Master_Style.add_fild}` }>
+                        <div className={ `${Add_Master_Style.add_all_fild}` }>
+                            <label>Id</label>
+                            <input
+                                value={ props.party_master.id }
+                                disabled
+                                type="text" />
+                        </div>
+
+                        <div className={ `${Add_Master_Style.add_all_fild}` }>
+                            <label>Khata name</label>
+                            <input
+                                value={ props.party_master.party_name }
+                                placeholder='Enter a party name'
+                                name='party_name'
+                                onChange={ props.party_change }
+                                type="text" />
+                        </div>
+
+                        <div className={ `${Add_Master_Style.add_all_fild}` }>
+                            <label>Group</label>
+                            <input
+                                value={ props.party_master.group_name }
+                                placeholder='Enter a group name'
+                                name='group_name'
+                                onChange={ props.party_change }
+                                type="text" />
+                        </div>
+
+                        <div
+                            style={ { margin: "0" } }
+                            className={ `${Add_Master_Style.add_all_fild}` }>
+                            <button onClick={ props.save_data }>{ props.edit ? "Update" : "Save" }</button>
+                            <button
+                                onClick={ props.click_show_list }
+                                className='secondary'>All List</button>
+                            <Link href="/">
+                                <button className='warning'>Close</button>
+                            </Link>
+                        </div>
+                    </div>
+            }
+        </section>
+
+    )
+}
+
+export default Kharcha
---
+++ pages/master/kharchamaster.js
@@ -0,0 +1,179 @@
+import React, { useState } from 'react'
+import Kharcha from '../../components/kharcha'
+
+const Kharchamaster = ({ api, party_all }) => {
+
+    const [party_list, setParty_list] = useState(party_all || [])
+
+    const [party_master, setParty_master] = useState({
+        id: party_list.length + 1,
+        group_name: "",
+        party_name: "",
+    })
+    const party_change = (e) => {
+        setParty_master({ ...party_master, [e.target.name]: e.target.value })
+    }
+    const [all_list, setAll_list] = useState(false)
+    const click_show_list = () => {
+        setAll_list(true)
+    }
+    const close_list = () => {
+        setAll_list(false)
+    }
+
+    const data = {
+        id: party_master.id,
+        kharcha_group: party_master.group_name,
+        kharcha_name: party_master.party_name,
+        is_delete: false,
+    }
+
+    const [edit, setEdit] = useState(false)
+
+    const save_data = async () => {
+        if (edit == false) {
+
+            if (party_master.group_name != "" && party_master.party_name != "") {
+                const res = await fetch(`${api}Kharch/Add`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ ...data })
+                    })
+                const resdata = await res.json()
+                console.log(resdata);
+                if (resdata == "Add Succesfully") {
+                    const res = await fetch(`${api}Kharch/Getkharch`,
+                        {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ data: "ok" })
+                        })
+                    const party_all = await res.json()
+
+                    setParty_list(party_all)
+                    setParty_master({
+                        ...party_master,
+                        id: party_all.length + 1,
+                        group_name: "",
+                        party_name: "",
+                    })
+                }
+            }
+        } else {
+            if (party_master.group_name != "" && party_master.party_name != "") {
+                const res = await fetch(`${api}Kharch/Update`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ ...data })
+                    })
+                const resdata = await res.json()
+                console.log(resdata);
+                if (resdata == "Khanrchname Update") {
+
+                    const res_n = await fetch(`${api}Kharch/Getkharch`,
+                        {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ data: "ok" })
+                        })
+                    const party_all = await res_n.json()
+                    setEdit(false)
+                    setParty_list(party_all)
+                    setParty_master({
+                        ...party_master,
+                        id: party_all.length + 1,
+                        group_name: "",
+                        party_name: "",
+                    })
+                }
+            }
+        }
+    }
+
+    const delete_data = async (e) => {
+        const res = await fetch(`${api}Kharch/Delete`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ delete_data_id: e })
+            })
+        const resdata = await res.json()
+
+        if (resdata == "Delete Succesfully") {
+            setParty_list(party_list.filter(item => item.id !== e));
+        }
+    }
+
+
+
+    const edit_data = async (e) => {
+        setAll_list(false)
+        setEdit(true)
+        const res = await fetch(`${api}Kharch/Edit`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id: e })
+            })
+        const resdata = await res.json()
+        setParty_master({
+            ...party_master,
+            id: resdata.id,
+            group_name: resdata.kharcha_group,
+            party_name: resdata.kharcha_name,
+        })
+    }
+    return (
+        <div>
+            <h1>Broker Master</h1>
+            <Kharcha
+                party_master={ party_master }
+                party_change={ party_change }
+                all_list={ all_list }
+                click_show_list={ click_show_list }
+                close_list={ close_list }
+                save_data={ save_data }
+                party_list={ party_list }
+                delete_data={ delete_data }
+                edit_data={ edit_data }
+                edit={ edit }
+            />
+        </div>
+    )
+}
+
+export default Kharchamaster
+
+export async function getserversideprops() {
+    const res = await fetch(`${process.env.API}Kharch/Getkharch`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data: "ok" })
+        })
+    const party_all = await res.json()
+
+
+    return {
+        props: {
+            "api": process.env.API,
+            "party_all": party_all,
+        }
+    }
+}

0987acd2a9c6aa189936e90fc7f85ccd2251f26c
Nikunj Mavani <nikung.bvminfotech@gmail.com>
2022-11-22 13:31:35 +0530

    asasas

--- pages/transaction/purchase/index.js
+++ pages/transaction/purchase/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/router";
-// import toast from 'react-hot-toast';
-import Purchase from '../../components/purchase'
+import Purchase from '../../../components/purchase'
 
 const Index = ({ resdata, api }) => {
 

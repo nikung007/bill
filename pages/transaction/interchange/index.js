ae7c09e02f921a377c2b1f1d4e1cf3e1b9bcc60c
Nikunj Mavani <nikung.bvminfotech@gmail.com>
2022-11-23 12:24:09 +0530

    sss

--- pages/transaction/interchange/index.js
+++ pages/transaction/interchange/index.js
@@ -122,7 +122,7 @@ const Index = ({ api, bank }) => {
 
 export default Index
 
-export async function getserversideprops({ query }) {
+export async function getstaticprops({ query }) {
 
     const res = await fetch(`${process.env.API}Banktransfer/GetBankname`,
         {

40349a900d4cd0f1681c789a50ab696ef1c68fa6
Nikunj Mavani <nikung.bvminfotech@gmail.com>
2022-11-25 00:25:45 +0530

    13

---
+++ pages/transaction/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex consequatur, quo sint saepe nesciunt eveniet molestias nostrum adipisci placeat corporis rerum asperiores vitae voluptatem officia vel laborum! Explicabo, nihil quas?
+        </div>
+    )
+}
+
+export default index

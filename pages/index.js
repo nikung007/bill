f96f4cca06f628e8a31cac88d684921e23aab599
Nikunj Mavani <nikung.bvminfotech@gmail.com>
2022-12-05 20:23:08 +0530

    5-12-2022

--- pages/index.js
+++ pages/index.js
@@ -10,26 +10,26 @@ export default function Home({ data }) {
         <div className={ `${Dashboard_Style.dashboard_main}` }>
           <div className={ `card ${Dashboard_Style.dashboard_card}` }>
             <h3 style={ { textAlign: "center" } } >FD-DOLLAR</h3>
-            <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.fddollar) * 100) / 100 } $</h3>
+            <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.fddollar) * 100) / 100 }</h3>
           </div>
           <div className={ `card ${Dashboard_Style.dashboard_card}` }>
             <h3 style={ { textAlign: "center" } } >FD-DIRAM</h3>
-            <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.fddiram) * 100) / 100 } د.إ </h3>
+            <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.fddiram) * 100) / 100 }</h3>
           </div>
           <div className={ `card ${Dashboard_Style.dashboard_card}` }>
             <h3 style={ { textAlign: "center" } } >FDFENILA-DIRAM</h3>
-            <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.fenildiram) * 100) / 100 } د.إ </h3>
+            <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.fenildiram) * 100) / 100 } </h3>
           </div>
           <div className={ `card ${Dashboard_Style.dashboard_card}` }>
-            <h3 style={ { textAlign: "center" } } >BUY-ROUGH</h3>
+            <h3 style={ { textAlign: "center" } } >TOTAL-BUY-ROUGH</h3>
             <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.buyrough) * 100) / 100 }</h3>
           </div>
           <div className={ `card ${Dashboard_Style.dashboard_card}` }>
-            <h3 style={ { textAlign: "center" } } >SELL-ROUGH</h3>
+            <h3 style={ { textAlign: "center" } } >TOTAL-SELL-ROUGH</h3>
             <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.sellrough) * 100) / 100 }</h3>
           </div>
           <div className={ `card ${Dashboard_Style.dashboard_card}` }>
-            <h3 style={ { textAlign: "center" } } >DIFF-ROUGH</h3>
+            <h3 style={ { textAlign: "center" } } >PADEL-ROUGH</h3>
             <h3 style={ { textAlign: "center", fontWeight: "700" } } >{ Math.round((data.differnceRouhg) * 100) / 100 }</h3>
           </div>
           <div className={ `card ${Dashboard_Style.dashboard_card}` }>
